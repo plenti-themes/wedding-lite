9fbb6d9d679e23bf5b290baec015dc2761110086
jimafisk <jimafisk@gmail.com>
2023-06-25 22:24:48 -0400

    Begin gallery style.

--- layouts/components/gallery.svelte
+++ layouts/components/gallery.svelte
@@ -1,10 +1,14 @@
+<script>
+    export let title, photos;
+</script>
+
 <section class="gallery-section section-bg section-padding" id="section_5">
     <div class="container">
         <div class="row">
 
             <div class="col-lg-12 col-12">
                 <div class="section-title-wrap mb-5">
-                    <h2 class="section-title">Gallery</h2>
+                    <h2 class="section-title">{title}</h2>
 
                     <div class="section-title-bottom">
                         <span class="section-title-line"></span>
@@ -14,72 +18,37 @@
                 </div>
             </div>
 
-            <div class="col-lg-4 col-12">
-                <div class="gallery-thumb image-hover-thumb">
-                    <a href="images/gallery/bearded-stylish-groom-suit-beautiful-blonde.jpg" class="popup-image">
-                        <img src="images/gallery/bearded-stylish-groom-suit-beautiful-blonde.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 d-flex flex-column">
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/couple.jpg" class="popup-image">
-                        <img src="images/gallery/couple.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/groom-with-bride-are-knocking-glasses-with-champagne.jpg" class="popup-image">
-                        <img src="images/gallery/groom-with-bride-are-knocking-glasses-with-champagne.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12">
-                <div class="gallery-thumb image-hover-thumb">
-                    <a href="images/gallery/bearded-stylish-groom-suit-beautiful-blonde.jpg" class="popup-image">
-                        <img src="images/gallery/bearded-stylish-groom-suit-beautiful-blonde.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 d-flex flex-column">
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/bride-groom-having-their-wedding-with-guests-beach.jpg" class="popup-image">
-                        <img src="images/gallery/bride-groom-having-their-wedding-with-guests-beach.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/wedding-ceremony-place-nature-outside-summer.jpg" class="popup-image">
-                        <img src="images/gallery/wedding-ceremony-place-nature-outside-summer.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12">
-                <div class="gallery-thumb image-hover-thumb">
-                    <a href="images/gallery/elegant-young-woman-fashionable.jpg" class="popup-image">
-                        <img src="images/gallery/elegant-young-woman-fashionable.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 d-flex flex-column">
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/young-couple-broke-down-car-while-traveling.jpg" class="popup-image">
-                        <img src="images/gallery/young-couple-broke-down-car-while-traveling.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
-
-                <div class="gallery-thumb gallery-thumb-small image-hover-thumb">
-                    <a href="images/gallery/young-couple-traveling-car-sunny-day.jpg" class="popup-image">
-                        <img src="images/gallery/young-couple-traveling-car-sunny-day.jpg" class="gallery-image img-fluid" alt="">
-                    </a>
-                </div>
+            <div class="photos">
+                {#each photos as photo, i}
+                    <div class="thumb {['one', 'two', 'three'][i % 3]} {i % 2 === 0 ? 'even' : 'odd'}">
+                        <img src="{photo.src}" alt="{photo.alt}">
+                    </div>
+                {/each}
             </div>
 
         </div>
     </div>
 </section>
+
+<style>
+    .photos {
+        display: grid;
+        grid: auto / repeat(3, 1fr);
+        gap: 20px;
+    }
+    .thumb {
+        border-radius: var(--border-radius-medium);
+        position: relative;
+        overflow: hidden;
+    }
+    .thumb.one {
+        grid-row: span 2;
+    }
+    .thumb.two, .thumb.three {
+        grid-row: span 1;
+    }
+    img {
+        max-width: 100%;
+        transform: scale(1.2);
+    }
+</style>
