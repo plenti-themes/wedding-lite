8152c3488a8ac94b478c19ae7f1f08ffdc839b24
jimafisk <jimafisk@gmail.com>
2023-06-27 14:35:17 -0400

    Add CMS login.

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -2,21 +2,28 @@
   import Head from "./head.svelte";
   import Nav from "./nav.svelte";
   import Footer from "./footer.svelte";
+  import Login from "./login.svelte";
 
-  export let content, layout, allContent, allLayouts, env;
+  export let content, layout, allContent, allLayouts, env, user;
 </script>
 
 <html lang="en">
   <Head title={content.fields.title} {env} />
   <body>
-    <Nav />
-    <svelte:component
-      this={layout}
-      {...content.fields}
-      {content}
-      {allContent}
-      {allLayouts}
-    />
-    <Footer />
+    {#if user && $user.isAuthenticated}
+      <svelte:component this={$user.menu} {user} bind:content={content} />
+    {/if}
+    <Login {user} />
+    <main>
+      <Nav />
+      <svelte:component
+        this={layout}
+        {...content.fields}
+        {content}
+        {allContent}
+        {allLayouts}
+      />
+      <Footer />
+    </main>
   </body>
 </html>
